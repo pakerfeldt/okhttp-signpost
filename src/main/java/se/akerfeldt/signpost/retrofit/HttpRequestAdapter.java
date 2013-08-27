b6ff49d1ccbbc6c6129299d60f22255664502783
Patrik Åkerfeldt <pa@spotify.com>
2013-08-27 08:51:26 +0200

    Some minor tweaks. Adding a helper client and a readme file

--- src/main/java/se/akerfeldt/signpost/retrofit/HttpRequestAdapter.java
+++ src/main/java/se/akerfeldt/signpost/retrofit/HttpRequestAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 Patrik Åkerfeldt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.akerfeldt.signpost.retrofit;
 
 import java.io.IOException;
@@ -6,16 +21,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import oauth.signpost.http.HttpRequest;
+
 import retrofit.client.Header;
 import retrofit.client.Request;
 
-public class HttpRequestAdapter implements oauth.signpost.http.HttpRequest {
+public class HttpRequestAdapter implements HttpRequest {
 
+	private static final String DEFAULT_CONTENT_TYPE = "application/json";
+	
 	private Request request;
 
-	public HttpRequestAdapter(retrofit.client.Request request) {
-        this.request = request;
+	private String contentType;
+
+	public HttpRequestAdapter(Request request) {
+        this(request, DEFAULT_CONTENT_TYPE);
     }
+	
+	public HttpRequestAdapter(Request request, String contentType) {
+		this.request = request;
+		this.contentType = contentType;
+	}
 	
 	@Override
 	public Map<String, String> getAllHeaders() {
@@ -28,7 +54,7 @@ public class HttpRequestAdapter implements oauth.signpost.http.HttpRequest {
 
 	@Override
 	public String getContentType() {
-		return "application/json";
+		return contentType;
 	}
 
 	@Override
